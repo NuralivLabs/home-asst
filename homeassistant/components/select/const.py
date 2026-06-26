13d0571c7f4686de704efbe79ec826913793a3e7
sudarsan-monkci <sudarsan.sr71@gmail.com>
2026-06-26 11:23:51 +0530

    Import home-assistant/core for agent testing

---
+++ homeassistant/components/select/const.py
@@ -0,0 +1,14 @@
+"""Provides the constants needed for the component."""
+
+DOMAIN = "select"
+
+ATTR_CYCLE = "cycle"
+ATTR_OPTIONS = "options"
+
+CONF_CYCLE = "cycle"
+CONF_OPTION = "option"
+
+SERVICE_SELECT_FIRST = "select_first"
+SERVICE_SELECT_LAST = "select_last"
+SERVICE_SELECT_NEXT = "select_next"
+SERVICE_SELECT_PREVIOUS = "select_previous"
---
+++ homeassistant/components/text/const.py
@@ -0,0 +1,10 @@
+"""Provides the constants needed for the component."""
+
+DOMAIN = "text"
+
+ATTR_MAX = "max"
+ATTR_MIN = "min"
+ATTR_PATTERN = "pattern"
+ATTR_VALUE = "value"
+
+SERVICE_SET_VALUE = "set_value"
