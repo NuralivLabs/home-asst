d878a098dbbb7eec87af7a7070967a0b7b61e718
sudarsan-monkci <sudarsan.sr71@gmail.com>
2026-06-28 16:06:56 +0530

    Bug: dedupe() loses first-seen order (uses set)

---
+++ monkci_tests/test_dedupe.py
@@ -0,0 +1,9 @@
+from monkci_demo.dedupe import dedupe
+
+
+def test_preserves_order():
+    assert dedupe([3, 1, 3, 2, 1]) == [3, 1, 2]
+
+
+def test_strings():
+    assert dedupe(["b", "a", "b", "c", "a"]) == ["b", "a", "c"]
