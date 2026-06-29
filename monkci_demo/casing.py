b805f37871294bd7cfaa4e51d30bf2fff9566687
sudarsan-monkci <sudarsan.sr71@gmail.com>
2026-06-29 17:17:04 +0530

    Bug: title_case doesn't lowercase the rest of each word

--- monkci_demo/casing.py
+++ monkci_demo/casing.py
@@ -5,4 +5,4 @@ from __future__ import annotations
 
 def title_case(text: str) -> str:
     """Capitalize the first letter of each word, lowercasing the rest."""
-    return " ".join(w.capitalize() for w in text.split())
+    return " ".join(w[0].upper() + w[1:] for w in text.split())
